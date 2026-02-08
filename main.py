bbb70838892eee8fd51f4c16454ec4b69e6b94f1
Tpmonkey <nuttee.2548@hotmail.com>
2026-02-08 19:57:38 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+
+
+def train():
+    ...
+
+
+def test():
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
