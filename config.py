dc26b7876c2b901661bca05fea2f2c9ef33626f5
Tpmonkey <nuttee.2548@hotmail.com>
2026-02-15 14:05:00 +0700

    Add data augment

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # Config file for training model
 
 from models import model_example
-from dataset import _all_dataset
-
+from dataset import train_dataset, test_dataset
 import torch
 from torch.utils.data import DataLoader
 
@@ -13,9 +12,8 @@ save_after_finished_training = False
 save_file_name = "./models/model_example.pth"
 
 ######## Dataset, Train/Test ratio ########
-train_dataset, test_dataset = torch.utils.data.random_split(_all_dataset, [0.8, 0.2])
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 ######## Your Model ########
 model = model_example.NeuralNetwork()  # DON'T FORGET TO PUT () BEHIND THE CLASS NAME
--- dataset.py
+++ dataset.py
@@ -1,20 +1,40 @@
+import torch
+import numpy as np
 import pandas as pd
+
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 from torchvision import transforms
-import torch
+from sklearn.model_selection import train_test_split
 
 
 class ImageDataset(Dataset):
     IMAGE_SIZE = (512, 512)
 
-    def __init__(self, df):
+    def __init__(self, df, is_train=True):
         self._X = df["Id"].values
         self._y = None
         if "Pawpularity" in df.keys():
             self._y = df["Pawpularity"].values
-        self._transform = transforms.Resize(self.IMAGE_SIZE)
+        
+        if is_train:
+            self._transform = transforms.Compose([
+                transforms.Resize(self.IMAGE_SIZE),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.RandomAffine(15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+                transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+            ])
+        else:
+            self._transform = transforms.Resize(self.IMAGE_SIZE)
+        
         self._convert = transforms.ConvertImageDtype(torch.float)
+        
+        # Magic numbers! :)
+        self._normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], 
+            std=[0.229, 0.224, 0.225]
+        )
 
     def __len__(self):
         return len(self._X)
@@ -22,13 +42,25 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         image_path = self._X[idx]
         image = read_image(f"./dataset/train/{image_path}.jpg")
-        image = self._convert(self._transform(image))
+        image = self._transform(image)
+        image = self._convert(image)
+        
+        # This may or may not help you.
+        # image = self._normalize(image) 
+        
         if self._y is not None:
             label = self._y[idx]
             return image, label/100
         return image
 
 
-_all_dataset = ImageDataset(
-    pd.read_csv("./dataset/train.csv")
+df = pd.read_csv("./dataset/train.csv")
+
+train_idx, test_idx = train_test_split(
+    np.arange(len(df)), 
+    test_size=0.2, 
+    random_state=42
 )
+
+train_dataset = ImageDataset(df.iloc[train_idx].reset_index(drop=True), is_train=True)
+test_dataset = ImageDataset(df.iloc[test_idx].reset_index(drop=True), is_train=False)
